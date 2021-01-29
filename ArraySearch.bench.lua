b90ded9093354bfaceab0d2a05a409ecf2d007e0
boatbomber <arbitrarysoandso@gmail.com>
2021-01-29 13:41:16 -0500

    Add documentation to top comment

--- ArraySearch.bench.lua
+++ ArraySearch.bench.lua
@@ -1,10 +1,18 @@
 --[[
-WARNING: THESE TESTS RUN IN YOUR REAL ENVIRONMENT.
+
+|WARNING| THESE TESTS RUN IN YOUR REAL ENVIRONMENT. |WARNING|
+
 If your tests alter a DataStore, it will actually alter your DataStore.
 
 This is useful in allowing your tests to move Parts around in the workspace or something,
 but with great power comes great responsibility. Don't mess up your stuff!
---]]
+
+---------------------------------------------------------------------
+
+Documentation and Change Log:
+https://devforum.roblox.com/t/benchmarker-plugin-compare-function-speeds-with-graphs-percentiles-and-more/829912/1
+
+--------------------------------------------------------------------]]
 
 local N = 800
 
